cb226f31ad3c87fc9b84bf5b5e9cc9cef3ab4e23
Nur Aziz <sukabaru09@gmail.com>
2024-11-19 17:59:05 +0700

    update

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import tiktokDl from "./tiktokScraper";
 
-const APP_PORT = process.env.PORT || 3000;
+const APP_PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 const app = express();
 app.use(morgan("dev"));
